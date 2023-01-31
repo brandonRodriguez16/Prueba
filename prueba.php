2c4f18a9e1e0d84ab0318102b703df4521949bd4
brandonRodriguez16 <108163997+brandonRodriguez16@users.noreply.github.com>
2023-01-30 20:47:13 -0600

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+
+        <title>Boton de prueba</title>
+
+    </head>
+
+    <body>
+
+        <button style="color:red; background:blue;">
+            Prueba
+        </botton>
+
+    </body>
+
+</html>
